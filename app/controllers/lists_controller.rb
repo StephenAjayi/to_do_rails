7436fd6416461098d47632c61e44b009cd909595
stephen <lonelytreesl@yahoo.com>
2015-07-15 18:58:12 -0700

    add new and create views for List.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    render :index
+  end
+
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end 
+end
